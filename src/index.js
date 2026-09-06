9fbce96554c413f54c305c4a3e74a0a57840568e
cuentalecoapp <cuentale.co.app@gmail.com>
2026-09-05 19:41:09 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const flujoCajaRoutes = require("./routes/flujoCaja");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: false })); // Twilio manda los webhooks en este formato
 
 app.get("/api/salud", (req, res) => res.json({ ok: true }));
